6adfcea6c21a35abf5b89a63931074c95345a765
bootjp <oh@bootjp.me>
2018-07-30 22:03:08 +0900

    first commit

---
+++ q6_wip.sql
@@ -0,0 +1,6 @@
+SELECT * FROM user WHERE id='admin' AND pass='pass' or 'pass' ='pass';UPDATE user SET pass='pass' WHERE id='admin';
+-- UPDATE user SET pass='pass' WHERE id='admin';"
+
+
+"SELECT * FROM user WHERE id='admin' AND pass='pass';"
+
